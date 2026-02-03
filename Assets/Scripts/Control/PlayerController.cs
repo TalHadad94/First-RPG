c2d66c8e0561ecc965523df45047e6089b672f35
Tal <talsmd95@gmail.com>
2026-02-03 18:30:22 +0200

    Add combat system: Implement CombatTarget and Fighter classes, enhance PlayerController for combat interactions

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,12 +1,35 @@
-using RPG.Movement;
+using System;
 using UnityEngine;
 
+using RPG.Combat;
+using RPG.Movement;
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
 
         private void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -16,12 +39,16 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out RaycastHit hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
